f9aac463844921d4bcb906e10ab9fd690f4d3c34
Wiibleyde <nathan@bonnell.fr>
2024-07-13 21:36:24 +0200

    refactor: Rework nightClub beamChannels.lua
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- nightClub/beamChannels.lua
+++ nightClub/beamChannels.lua
@@ -1,4 +1,5 @@
 local lights = {
+    -- Left side
     {
         id = "1",
         type = "column",
@@ -19,38 +20,132 @@ local lights = {
     },
     {
         id = "4",
-        type = "diagonnal",
+        type = "column",
         side = "left",
         level = "bottom",
     },
     {
         id = "5",
-        type = "column",
+        type = "diagonnal",
         side = "left",
         level = "top",
     },
     {
         id = "6",
-        type = "diagonnal",
-        side = "left",
-        level = "top",
-    },
-    {
-        id = "7",
         type = "column",
         side = "left",
         level = "top",
+    },
+    -- Back side
+    {
+        id = "7",
+        type = "column",
+        side = "back",
+        level = "bottom",
     },
     {
         id = "8",
         type = "diagonnal",
-        side = "left",
-        level = "top",
+        side = "back",
+        level = "bottom",
     },
     {
         id = "9",
         type = "column",
+        side = "back",
+        level = "bottom",
+    },
+    {
+        id = "10",
+        type = "diagonnal",
+        side = "back",
+        level = "bottom",
+    },
+    {
+        id = "11",
+        type = "column",
+        side = "back",
+        level = "top",
+    },
+    {
+        id = "12",
+        type = "diagonnal",
+        side = "back",
+        level = "top",
+    },
+    {
+        id = "13",
+        type = "column",
+        side = "back",
+        level = "top",
+    },
+    {
+        id = "14",
+        type = "diagonnal",
+        side = "back",
+        level = "top",
+    },
+    {
+        id = "15",
+        type = "column",
+        side = "back",
+        level = "top",
+    },
+    -- Right side
+    {
+        id = "16",
+        type = "column",
         side = "right",
         level = "bottom",
-    },    -- ...
+    },
+    {
+        id = "17",
+        type = "diagonnal",
+        side = "right",
+        level = "bottom",
+    },
+    {
+        id = "18",
+        type = "column",
+        side = "right",
+        level = "bottom",
+    },
+
+    {
+        id = "19",
+        type = "diagonnal",
+        side = "right",
+        level = "bottom",
+    },
+    {
+        id = "20",
+        type = "column",
+        side = "right",
+        level = "top",
+    },
+    {
+        id = "21",
+        type = "diagonnal",
+        side = "right",
+        level = "top",
+    },
+    
+    {
+        id = "22",
+        type = "column",
+        side = "right",
+        level = "top",
+    },
+    {
+        id = "23",
+        type = "column",
+        side = "right",
+        level = "top",
+    },
+    {
+        id = "24",
+        type = "diagonnal",
+        side = "right",
+        level = "top",
+    },
 }
